b5de9c7fa05e50ec38fa2030ea58d9a5d225fb2e
Joel AZEMAR <joel.azemar@gmail.com>
2013-10-14 16:13:35 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orion::Application.config.secret_key_base = 'a9cd47723dd6422b21104745fd3dad36422f732f6a9b02ae5a36bf36aedbf40b7d97fc6296a321f437cddf8ca2726f363e2df4f089bc6f8f9c338a2aab1eaf65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Orion::Application.config.session_store :cookie_store, key: '_orion_session'
